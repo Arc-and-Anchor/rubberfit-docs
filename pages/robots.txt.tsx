c7c8a8c737f39138eec31094fc72f264147f4efe
Daniel A Martinez Julio <dan.martinezjulio@gmail.com>
2026-05-12 18:50:07 -0700

    Add docs search metadata

---
+++ pages/robots.txt.tsx
@@ -0,0 +1,21 @@
+import type { GetServerSideProps } from "next";
+
+const robots = `User-agent: *
+Allow: /
+
+Sitemap: https://docs.rubberfit.app/sitemap.xml
+`;
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  res.setHeader("Content-Type", "text/plain");
+  res.write(robots);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default function Robots() {
+  return null;
+}
